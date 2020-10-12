7dea4d2f61b0ac989a6100887910a176a3d2b6c7
jucesarsilva <julio.silva.somos@gmail.com>
2020-10-12 10:30:20 -0300

    Updates

---
+++ precache-manifest.7bc10e1c774b1d09351871c431bd9d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c444d39b10e2f1aa6be5ea15fd54e4fe",
+    "url": "/point-control-react/index.html"
+  },
+  {
+    "revision": "d16f15d703551a71301e",
+    "url": "/point-control-react/static/css/2.e344ae16.chunk.css"
+  },
+  {
+    "revision": "fd120fc600148557516b",
+    "url": "/point-control-react/static/css/main.3a1c8776.chunk.css"
+  },
+  {
+    "revision": "d16f15d703551a71301e",
+    "url": "/point-control-react/static/js/2.8bdcaff6.chunk.js"
+  },
+  {
+    "revision": "fd120fc600148557516b",
+    "url": "/point-control-react/static/js/main.0058b1f3.chunk.js"
+  },
+  {
+    "revision": "52251854b0cecdf51f78",
+    "url": "/point-control-react/static/js/runtime~main.4777023b.js"
+  }
+]);
